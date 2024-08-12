0c861f65086d54d23be464d22be1e16b71592fa5
ivan <rusansky.in@gmail.com>
2024-08-12 18:15:08 -0300

    act 5

--- nexacoreapp/src/components/ItemDetailContainer.jsx
+++ nexacoreapp/src/components/ItemDetailContainer.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
